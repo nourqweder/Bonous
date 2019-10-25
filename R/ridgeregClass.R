cd138ba51b0c737f2697508ba2733594fe8a1a98
Nour Elhouda Qweder <nouqw898@student.liu.se>
2019-10-25 22:27:31 +0200

    update class and calculate method add print class and try to fix printing stuff still not finsh yet

--- R/ridgeregClass.R
+++ R/ridgeregClass.R
@@ -1,115 +1,119 @@
 #' Ridge regression class
+#' @title Ridge regression
 #'
 #'
-#' @description
 #' ridgereg(formula, data, lambda).
 #' Ridge regression can be a good alternative when we have a lot of covariates (when p > n) or in the
 #' situation of multicollinearity. More information on ridge regression can be found in chapter 3.4.1 in [1].
-#' @field lambda argument lambda a constant for ridge
-#' @field myFormula formula, is a object of class formula.
+#' @field lambda argument lambda a constant for ridge. "a hyperparameter"
+#' @field myFormula formula, is a object of class formula. format y ~ x_1 + x_2 + ... + x_n.
 #' @field myData character, The data is typically a data.frame. The gieven data set.
-#'
 #' @references Reference Classes: \url{http://adv-r.had.co.nz/R5.html}
-#'
-#' QR decomposition: \url{https://en.wikipedia.org/wiki/QR_decomposition}
-#'
-#' Vignettes: \url{http://r-pkgs.had.co.nz/vignettes.html}
-#'
-#' @importFrom methods new
-#'
+#' @description Returns the result of the Ridge Regression
+#' @examples
+#' data(iris)
+#' ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris, lambda = 0)$print()
+#' ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris, lambda = 0)$predict()
+#' ridgereg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris, lambda = 0)$coef()
 #' @export ridgereg
-#' @exportClass ridgereg
+#' @export
 #'
 ridgereg <- setRefClass("ridgereg",
-                      fields = list(
-                         #x = "ANY", #Independent Values (is considered to be an explaantory variable)
-                         #y = "ANY", #Depenent Values (whose value we want to explain or forecast)
-                         #betaHat ="matrix", #
-                         #fittedValues = "matrix",
-                         #epsilon = "matrix",
-                         #n = "numeric",
-                         #p = "numeric",
-                         #df = "numeric",
-                         #segmaSequare = "matrix",
-                         #regressionsVar = "matrix",
-                         #tValue = "matrix",
-                         #pValues = "numeric",
+                          fields = list(
                         formula = "formula",
-                        data = "character",
-                         lambda= "numeric"
+                        data = "data.frame",
+                        lambda= "numeric"
                         ),
                          methods= list(
 
                          #--------------------------------------------------------------------------------------
                          #ridgereg(formula, data, lambda)
-                         initialize <- function(formula, data,lambda ){
-                          #,normalise = TRUE) error in initilize function
-
-                           #1- check validation
-                          # ifelse(!is.data.frame(data),stop("the input data is not valid"),
-                           #       ifelse(!class(formula) != "formula"), stop("formula is not valid"),
-                            #      ifelse(!all(sapply(data[,conames(data)%in% all.vars(formula)],is.numeric)), stop("not numeric data"),
-                            #             ifelse(!length(lambda)==1, stop("lambda not valid")," ")))
-
+                         initialize = function(formula, data,lambda ){
+                           # 1- Check validation & initialize
+                           stopifnot(is.data.frame(data), length(lambda) == 1, class(formula) == "formula")
                            data <<-data
                            formula <<-formula
                            lambda <<-lambda
+                           normalise <- TRUE
+
                          },
                          #--------------------------------------------------------------------------------------
                          #2- calculate values
                          #The goal here is to establish a mathematical equation for a ridge regression model
-                         calculateValues <- function()
-                         {
+                          calculateValues = function()
+                          {
                            result <- list()
-                           x <<- model.matrix(formula, data)
-                           y<-data[,colnames(data)==all.vars(formula)[1]]
-                           #NROMLAIZED
-                           x[,2:ncol(x)] <- scale(x[,-1])
-                           # is speeed in cars data
+
+                           # Define matrix
+                           X <- model.matrix(formula, data)# x is the independent variables
+
+                           # 1. Normlization
+                           X[,2:ncol(X)] <- scale( X[,-1])
+
+                           # 2. Calculations using least squares.
+                           dependentVar <- all.vars(expr = formula)[1]
+                           y <- data[, dependentVar] # y is the dependent variable/s in this formula
+                           I <- diag(ncol(x))
+
+
+                           ##3.  Compute Estimations
+                           # Regression coefficients:
                            #betaHat <<- coef(lm(y ~ x))
-                           # Regression Coefficients
-                           result$coef <- solve(t(x) %*% x+ diag(lambda, nrow = ncol(x))) %*% (t(x) %*% y)
+
+                           result$coef  <- as.vector(solve( t(x) %*% x + lambda * I) %*% t(x) %*% y)
+
                            # yHat = X*bHat  === yhat = beta0 + beta1*x
-                           #fittedValues <<- x%*%betaHat
-                           result$fittedValues <- x%*%result$coef
+                           # The fitted values:
+                           #betaZero <- mean(y)
+                           result$fittedValues <- x %*% result$coef
                            return(result)
-                         }#,
+                         },
                          #--------------------------------------------------------------------------------------
-                         #print=function()
-                         #{
-                          # "print out the coefficients and coefficient names, similar as done by the lm class."
-
-                         #},
+                          print=function()
+                         {
+                          "print out the coefficients and coefficient names, similar as done by the lm class."
+                           cat(paste("call: \n"))
+                           cat(paste("ridgereg(formula = ", deparse(formula), ", data = ", deparse(substitute(data))," lambda = ", lambda, ")\n\n", sep = ""))
+                           cat(paste("Coefficients:\n"))
+                           info <- calculateValues()[[1]]
+                           structure( c(info), names = row.names(info))
+                         }))
                          #--------------------------------------------------------------------------------------
-                         #predict = function()
-                         #{
-                          # "return the predicted values ^y, it should be able to predict for new dataset similar"
+ridgereg$methods(predict = function()
+                         {
+                          "return the predicted values ^y, it should be able to predict for new dataset similar"
 
-                         #},
+                         })
                          #--------------------------------------------------------------------------------------
-                        # coef = function()
-                         #{#^ ridge =( XTX + I)1
+ridgereg$methods(coef = function()
+                        {#^ ridge =( XTX + I)1
 
-                          # "return the ridge regression coefficients ^ ridge"
+                         "return the ridge regression coefficients ^ ridge"
 
-                         #}
-                       ))
+                        })
 
 
-#lambda <- 1
-#data <- iris[,1:4]
-#formula <- Sepal.Length ~ Sepal.Width + Petal.Length
-#x<-model.matrix(formula, data)
-#head(x)
-#formula <- Sepal.Length ~ Sepal.Width + Petal.Length
+
+
+
+lambda <- 1
+
+data <- iris[,1:4]
+formula <- Sepal.Length ~ Sepal.Width + Petal.Length
+class(formula)
+x<-model.matrix(formula, data)
+head(x)
+formula <- Sepal.Length ~ Sepal.Width + Petal.Length
+o <- ridgereg$new(formula, data,lambda)
+o$print()
 #X<-model.matrix(formula, data)
 #x[,2:ncol(x)] <- scale(x[,-1]) #Normalizing
 #head(X)
-#calculateValues()
-#traceback()
-#epal.Length
-#(Intercept)       3322182
-#Sepal.Width      10061570
-#Petal.Length     13971232
 
+calculateValues()
+traceback()
+
+#lm.ridge(formula, data,lambda = lambda)
 #lm.ridge(formula, data)
+data(flights)
+#o <- ridgereg$new(formula, data)
